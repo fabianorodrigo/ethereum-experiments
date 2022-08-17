f976a92898706f7d45a8fcb9331d4046afacad78
Fabiano Nascimento <fabianorodrigo@gmail.com>
2022-08-17 09:54:53 -0300

    learnWeb3: Inclusão de um teste que força Transaction ran out of gas

--- test/learnweb3/randomness.ts
+++ test/learnweb3/randomness.ts
@@ -11,18 +11,38 @@ describe("Randomness Attack", function () {
 
     console.log("Game contract's address:", _game.address);
 
-    //Deploy attack contract
+    // Deploy attack contract
     const Attack = await ethers.getContractFactory("RandomnessAttack");
     const _attack = await Attack.deploy(_game.address);
 
     console.log("Attack contract's address:", _attack.address);
 
-    //Attack the Game Contract
+    // Attack the Game Contract
     const tx = await _attack.attack();
     await tx.wait();
 
     const balanceGame = await _game.getBalance();
     // Balance should be zero
     expect(balanceGame).to.equal(ethers.constants.Zero);
+  });
+
+  it("Should revert with not enough gas is sent", async function () {
+    // deploy the Game contract
+    const Game = await ethers.getContractFactory("Game");
+    const _game = await Game.deploy({ value: utils.parseEther("0.1") });
+    await _game.deployed();
+
+    console.log("Game contract's address:", _game.address);
+
+    // Deploy attack contract
+    const Attack = await ethers.getContractFactory("RandomnessAttack");
+    const _attack = await Attack.deploy(_game.address);
+
+    console.log("Attack contract's address:", _attack.address);
+
+    // Attack the Game Contract
+    await expect(_attack.attack({ gasLimit: 21064 })).to.be.revertedWith(
+      "TransactionExecutionError: Transaction ran out of gas"
+    );
   });
 });
