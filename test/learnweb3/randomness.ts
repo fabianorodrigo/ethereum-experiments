eaebcb702239d4335abdff5bb8a75c572d13a38d
Fabiano Nascimento <fabianorodrigo@gmail.com>
2022-10-15 11:44:07 -0300

    Testes com gasLimit insuficiente

--- test/learnweb3/randomness.ts
+++ test/learnweb3/randomness.ts
@@ -9,13 +9,9 @@ describe("Randomness Attack", function () {
     const _game = await Game.deploy({ value: utils.parseEther("0.1") });
     await _game.deployed();
 
-    console.log("Game contract's address:", _game.address);
-
     // Deploy attack contract
     const Attack = await ethers.getContractFactory("RandomnessAttack");
     const _attack = await Attack.deploy(_game.address);
-
-    console.log("Attack contract's address:", _attack.address);
 
     // Attack the Game Contract
     const tx = await _attack.attack();
@@ -32,19 +28,30 @@ describe("Randomness Attack", function () {
     const _game = await Game.deploy({ value: utils.parseEther("0.1") });
     await _game.deployed();
 
-    console.log("Game contract's address:", _game.address);
+    // Deploy attack contract
+    const Attack = await ethers.getContractFactory("RandomnessAttack");
+    const _attack = await Attack.deploy(_game.address);
+
+    // TODO: entender por que mesmo passando 21.064 ainda falha: https://ethereum.stackexchange.com/questions/133728/msg-sender-callvalue-0-1-ether-gas-1-why-it-doesnt-fail-shouldnt
+    // Valida a mensagem de erro retornada pela Hardhat Network: https://github.com/NomicFoundation/hardhat/blob/main/packages/hardhat-core/src/internal/hardhat-network/provider/node.ts
+    await expect(_attack.attack({ gasLimit: 21064 })).to.be.rejectedWith(
+      "Transaction ran out of gas"
+    );
+  });
+
+  it("Should revert when transaction sent with less than 21064", async function () {
+    // deploy the Game contract
+    const Game = await ethers.getContractFactory("Game");
+    const _game = await Game.deploy({ value: utils.parseEther("0.1") });
+    await _game.deployed();
 
     // Deploy attack contract
     const Attack = await ethers.getContractFactory("RandomnessAttack");
     const _attack = await Attack.deploy(_game.address);
 
-    console.log("Attack contract's address:", _attack.address);
-
-    // Attack the Game Contract
-
-    //console.log(await _attack.attack({ gasLimit: 1 }));
-    await expect(_attack.attack({ gasLimit: 21064 })).to.be.revertedWith(
-      "TransactionExecutionError: Transaction ran out of gas"
+    // Valida a mensagem de erro retornada pela Hardhat Network: https://github.com/NomicFoundation/hardhat/blob/main/packages/hardhat-core/src/internal/hardhat-network/provider/node.ts
+    await expect(_attack.attack({ gasLimit: 1 })).to.be.rejectedWith(
+      "Transaction requires at least 21064 gas but got 1"
     );
   });
 });
