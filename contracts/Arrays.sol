048ab5ecd61db8cfd0366fd16ad555c5c6378efb
Fabiano Nascimento <fabianorodrigo@gmail.com>
2022-10-16 07:39:33 -0300

    Remoção de console.log do initStorage e substituição do uint por uint256

--- contracts/Arrays.sol
+++ contracts/Arrays.sol
@@ -4,34 +4,37 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Arrays {
+    uint256[] public arrayStorage;
 
-	uint[] public arrayStorage;
-
-    function initStorage(uint length) public returns(uint) {
-		arrayStorage = new uint[](length);
-        console.log("arrayStorage.length",arrayStorage.length);
-        return arrayStorage.length;
-	}
-
-    function getArrayStorageLength() public view returns(uint){
+    function initStorage(uint256 length) public returns (uint256) {
+        arrayStorage = new uint256[](length);
+        //console.log("arrayStorage.length",arrayStorage.length);
         return arrayStorage.length;
     }
-	
-	function initMemoryLiteralOrConstant() public view returns(uint) {
-		uint[10] memory arrayMemory;
-		console.log("arrayMemory.length",arrayMemory.length);
-		return arrayMemory.length;
-	}
 
-	function initMemoryDynamically(uint length) public view returns(uint) {
-		uint[] memory arrayMemory = new uint[](length);
-		console.log("arrayMemory.length",arrayMemory.length);
-		return arrayMemory.length;
-	}
+    function getArrayStorageLength() public view returns (uint256) {
+        return arrayStorage.length;
+    }
 
-    function initUnsizedMemory() public view returns(uint){
-		uint[] memory arrayMemory;
-        console.log("arrayMemory.length",arrayMemory.length);
-		return arrayMemory.length;
-	}
+    function initMemoryLiteralOrConstant() public view returns (uint256) {
+        uint256[10] memory arrayMemory;
+        console.log("arrayMemory.length", arrayMemory.length);
+        return arrayMemory.length;
+    }
+
+    function initMemoryDynamically(uint256 length)
+        public
+        view
+        returns (uint256)
+    {
+        uint256[] memory arrayMemory = new uint256[](length);
+        console.log("arrayMemory.length", arrayMemory.length);
+        return arrayMemory.length;
+    }
+
+    function initUnsizedMemory() public view returns (uint256) {
+        uint256[] memory arrayMemory;
+        console.log("arrayMemory.length", arrayMemory.length);
+        return arrayMemory.length;
+    }
 }
